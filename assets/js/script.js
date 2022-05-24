ef31e6db5d19107584345302ebc99e93029572c2
nrenner <nrenner0211@gmail.com>
2022-05-24 18:39:49 -0400

    delete function added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ console.dir(window.document);
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function (event) {
     event.preventDefault();
@@ -95,7 +96,21 @@ actionContainerEl.appendChild(statusSelectEl);
 
 formEl.addEventListener("submit", taskFormHandler);
 
+var taskButtonHandler = function(event) {
+    console.log(event.target);
 
+    if (event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+  };
+
+pageContentEl.addEventListener("click", taskButtonHandler);
 
 
 
